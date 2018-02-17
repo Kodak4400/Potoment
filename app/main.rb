cd138dbf6466d5afb9049fdb0fbb1071ab4444cb
初期登録 <Kodak@Kodak.local>
2018-02-17 16:48:39 +0900

    簡易バグ修正

--- app/main.rb
+++ app/main.rb
@@ -23,7 +23,7 @@ end
 def client
   @client ||= Line::Bot::Client.new { |config|
     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-    config.chaneel_token = ENV["LINE_CHANNEL_TOKEN"]
+    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
   }
 end
 
