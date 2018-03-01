e542e8b3dde60a06865880ca97d75da0313418ee
初期登録 <Kodak@Kodak.local>
2018-03-02 01:07:46 +0900

    no message

--- app/main.rb
+++ app/main.rb
@@ -105,14 +105,14 @@ get '/websocket' do
         settings.sockets << ws
       end
       # WebSocket通信によってメッセージが来た時の処理をまとめる
-      ws.onmessage do |msg|
+      # ws.onmessage do |msg|
         settings.sockets.each do |s|
           # WebSocket通信でsndする
           # WebSocket通信で、クライアントに向かって情報をおくる
           # sendメソッドでは、１クライアントにしか送れない。
           s.send(msg)
         end
-      end
+      # end
       # WebSocket通信が切断された時の処理をまとめる
       ws.onclose do
         # wsを削除
